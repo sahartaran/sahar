666726cc6325e0f9c72a2adf7b3ada76bdac3f20
Sahar taran <117488413+sahartaran@users.noreply.github.com>
2022-11-13 20:58:45 +0330

    Add files via upload

---
+++ P4.cxx
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+char x;
+cout<<"pleas enter x:";
+cin>>x;
+switch (x){
+case 'a':
+cout<<"vowel";
+break;
+case 'e':
+cout<<"vowel";
+break;
+case 'i':
+cout<<"vowel";
+break;
+case 'o':
+cout<<"vowel";
+break;
+case 'u':
+cout<<"vowel";
+break;
+default:cout<<"not vowel";
+}
+}
