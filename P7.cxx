215ff251a465be2db27df91ac3d38cbbee114545
Sahar taran <117488413+sahartaran@users.noreply.github.com>
2022-11-28 01:50:34 +0330

    Add files via upload

---
+++ P7.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"pleas enter division:";
+	cin>>n;
+	int divisor;
+	int quotient;
+	for(n%divisor==0){
+		quotient=quotient++;
+	}
+	
+	cout<<quotient;
+	
+}
